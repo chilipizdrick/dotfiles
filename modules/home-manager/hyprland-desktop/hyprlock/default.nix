2d97e20288f35a8505556dd2652aabc378f4f35a
Alexey Ershov <alexa3mail@gmail.com>
2024-08-11 16:44:55 +0300

    hyprlock shenanigans

--- modules/home-manager/hyprland-desktop/hyprlock/default.nix
+++ modules/home-manager/hyprland-desktop/hyprlock/default.nix
@@ -28,7 +28,7 @@ in {
         };
         background = [
           {
-            # monitor = "";
+            monitor = "";
             path = "$HOME/.current_wallpaper";
             blur_size = 4;
             blur_passes = 3;
@@ -41,105 +41,105 @@ in {
         ];
         input-field = [
           {
-            # monitor = "";
-            size = "250, 50";
+            monitor = "";
+            size = "${builtins.toString (250 * cfg.scale)}, ${builtins.toString (50 * cfg.scale)}";
             outline_thickness = 0;
             dots_size = 0.26;
             dots_spacing = 0.64;
             dots_center = true;
-            rounding = 22;
+            rounding = builtins.floor (22 * cfg.scale);
             outer_color = "rgba(000000AA)";
             inner_color = "rgba(000000AA)";
             font_color = "rgb(ffffff)";
             fade_on_empty = true;
             placeholder_text = "";
-            position = "0, 150";
+            position = "0, ${builtins.toString (150 * cfg.scale)}";
             halign = "center";
             valign = "bottom";
           }
         ];
         label = [
           {
-            # monitor = "";
+            monitor = "";
             text = "cmd[update:1000] echo \"<b><big> $(date +\"%H\") </big></b>\"";
             color = "rgb(ffffff)";
-            font_size = 112;
+            font_size = builtins.floor (112 * cfg.scale);
             font_family = "Geist Mono 10";
             shadow_passes = 3;
             shadow_size = 4;
-            position = "0, -100";
+            position = "0, ${builtins.toString (-100 * cfg.scale)}";
             halign = "center";
             valign = "top";
           }
           {
-            # monitor = "";
+            monitor = "";
             text = "cmd[update:1000] echo \"<b><big> $(date +\"%M\") </big></b>\"";
             color = "rgb(ffffff)";
-            font_size = 112;
+            font_size = builtins.floor (112 * cfg.scale);
             font_family = "Geist Mono 10";
             shadow_passes = 3;
             shadow_size = 4;
-            position = "0, -240";
+            position = "0, ${builtins.toString (-240 * cfg.scale)}";
             halign = "center";
             valign = "top";
           }
           {
-            # monitor = "";
+            monitor = "";
             text = "cmd[update:18000000] echo \"<b><big> $(date +\"%A\") </big></b>\"";
             color = "rgb(ffffff)";
-            font_size = 22;
+            font_size = builtins.floor (22 * cfg.scale);
             font_family = "JetBrainsMono Nerd Font 10";
-            position = "0, -450";
+            position = "0, ${builtins.toString (-450 * cfg.scale)}";
             shadow_passes = 3;
             shadow_size = 1;
             halign = "center";
             valign = "top";
           }
           {
-            # monitor = "";
+            monitor = "";
             text = "cmd[update:18000000] echo \"<b> $(date +\"%d %b\") </b>\"";
             color = "rgb(ffffff)";
-            font_size = 18;
+            font_size = builtins.floor (18 * cfg.scale);
             font_family = "JetBrainsMono Nerd Font 10";
             shadow_passes = 3;
             shadow_size = 1;
-            position = "0, -490";
+            position = "0, ${builtins.toString (-490 * cfg.scale)}";
             halign = "center";
             valign = "top";
           }
           {
-            # monitor = "";
+            monitor = "";
             text = ''cmd[update:250] echo "$(hyprctl devices -j | jq -r '.keyboards[] | select(.main == true) | .active_keymap')"'';
             color = "rgb(ffffff)";
-            font_size = 15;
+            font_size = builtins.floor (15 * cfg.scale);
             font_family = "JetBrainsMono Nerd Font 10";
             shadow_passes = 3;
             shadow_size = 1;
-            position = "0, 210";
+            position = "0, ${builtins.toString (210 * cfg.scale)}";
             halign = "center";
             valign = "bottom";
           }
           {
-            # monitor = "";
+            monitor = "";
             text = ''cmd[update:1000] [ $(playerctl status) = Playing ] && [ $(playerctl metadata | head -1 | awk '{print $1}') != firefox ] && [ $(playerctl metadata | head -1 | awk '{print $1}') != brave ] && echo "<b>$([ $(playerctl metadata | head -1 | awk '{print $1}') = spotify ] && echo ' ')$(playerctl metadata artist) - $(playerctl metadata title)</b>"'';
             color = "rgb(ffffff)";
-            font_size = 18;
+            font_size = builtins.floor (18 * cfg.scale);
             font_family = "JetBrainsMono Nerd Font 10";
             shadow_passes = 3;
             shadow_size = 1;
-            position = "0, 70";
+            position = "0, ${builtins.toString (70 * cfg.scale)}";
             halign = "center";
             valign = "bottom";
           }
           {
-            # monitor = "";
+            monitor = "";
             text = "cmd[update:18000000] echo \"<b>Feels like<big> $(curl -s 'wttr.in?format=%t' | tr -d '+') </big></b>\"";
             color = "rgb(ffffff)";
-            font_size = 18;
+            font_size = builtins.floor (18 * cfg.scale);
             font_family = "JetBrainsMono Nerd Font 10";
             shadow_passes = 3;
             shadow_size = 1;
-            position = "0, 20";
+            position = "0, ${builtins.toString (20 * cfg.scale)}";
             halign = "center";
             valign = "bottom";
           }
