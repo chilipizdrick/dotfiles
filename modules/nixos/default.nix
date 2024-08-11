bb532117595ad8a575ecd82c68d5e2f6ed48f4a4
Alexey Ershov <alexa3mail@gmail.com>
2024-08-11 17:07:27 +0300

    fix: hypridle at last

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -10,7 +10,7 @@
     ./games
     ./greeter
     ./hyperion
-    ./hyprland
+    # ./hyprland
     ./locale
     ./misc
     ./polkit
--- modules/nixos/xdg-portals/default.nix
+++ modules/nixos/xdg-portals/default.nix
@@ -3,11 +3,17 @@
     enable = true;
     extraPortals = with pkgs; [
       xdg-desktop-portal-gtk
+      xdg-desktop-portal-hyprland
     ];
     config = {
       common = {
         default = [
           "gtk"
+        ];
+      };
+      hyprland = {
+        default = [
+          "hyprland"
         ];
       };
     };
