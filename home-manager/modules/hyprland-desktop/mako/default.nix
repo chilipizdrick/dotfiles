f93a0bb0282ca4c3118852a5a7d8eeb09c2d7368
Alexey Ershov <alexa3mail@gmail.com>
2024-09-22 18:09:30 +0300

    change look of notifications

--- home-manager/modules/hyprland-desktop/mako/default.nix
+++ home-manager/modules/hyprland-desktop/mako/default.nix
@@ -4,10 +4,12 @@
     package = pkgs.mako;
     defaultTimeout = 5000;
     anchor = "top-right";
-    textColor = "#ffffff";
+    # textColor = "#ffffff";
+    textColor = "#cdd6f4"; # Catpuccin mocha text color
     backgroundColor = "#000000B3";
-    borderColor = "#ffffff";
-    borderSize = 0;
+    # borderColor = "#ffffff";
+    borderColor = "#89b4fa"; # Catpuccin mocha blue color
+    borderSize = 2;
     borderRadius = 10;
     font = "JetBrainsMonoNL Nerd Font";
     layer = "top";
