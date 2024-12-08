76af595bf69cdb8d12aa7d130cb08ca223234529
Alexey Ershov <alexa3mail@gmail.com>
2024-12-09 00:26:49 +0300

    remove hyprland killbind

--- home-manager/modules/hyprland-desktop/hyprland/default.nix
+++ home-manager/modules/hyprland-desktop/hyprland/default.nix
@@ -263,7 +263,7 @@ in {
         "${mod} ALT, R, exec, ${pkgs.scripts.reload-graphical-interface}/bin/reload-graphical-interface" # Refresh waybar, rofi
         "CTRL ALT, Delete, exec, hyprctl dispatch exit"
         "${mod} ALT, C, exec, hyprctl reload"
-        "${mod} ALT, E, exec, hyprctl dispatch exit"
+        # "${mod} ALT, E, exec, hyprctl dispatch exit"
         "${mod} ALT, L, exec, ${pkgs.hyprlock}/bin/hyprlock"
         "${mod} SHIFT, P, exec, pidof wlogout || ${pkgs.wlogout}/bin/wlogout -b 4"
         "${mod} SHIFT, A, exec, ${pkgs.scripts.toggle-hyprland-settings}/bin/toggle-hyprland-settings animations"
