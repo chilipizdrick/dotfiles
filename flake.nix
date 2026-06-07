59be70fb539d4a7eb9e31b583f8564276ec68d77
Alexey Ershov <alexa3mail@gmail.com>
2026-06-07 16:29:58 +0300

    Revert "sync to atlas"
    This reverts commit 78c057967d027e464ae5824713466637fcd7cf92.

--- flake.nix
+++ flake.nix
@@ -2,11 +2,12 @@
   description = "Personal NixOS & home-manager config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    # nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     flake-parts.url = "github:hercules-ci/flake-parts";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -23,6 +24,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # sl2.url = "github:chilipizdrick/sl2";
     wroomer.url = "github:chilipizdrick/wroomer";
     hijacker2.url = "github:chilipizdrick/hijacker2";
   };
--- home/modules/hyprland-desktop/qt/default.nix
+++ home/modules/hyprland-desktop/qt/default.nix
@@ -3,6 +3,10 @@
     enable = true;
     platformTheme.name = "kde";
     style.name = "breeze";
+    # iconTheme = {
+    #   name = "breeze-dark";
+    #
+    # };
   };
 
   xdg.configFile."kdeglobals".text = ''
