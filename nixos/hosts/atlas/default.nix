4661aad5a226fa68968e82692fac74090b2f483e
Alexey Ershov <alexa3mail@gmail.com>
2026-07-10 15:25:21 +0300

    small change for atlas

--- nixos/hosts/atlas/default.nix
+++ nixos/hosts/atlas/default.nix
@@ -21,6 +21,8 @@
     (btop.override {rocmSupport = true;})
   ];
 
+  programs.noctalia-greeter.settings.output.scale = 1.666667;
+
   services.upower.enable = true;
 
   networking.hostName = "atlas";
